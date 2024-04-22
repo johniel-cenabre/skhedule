f0dfde79a8ffa07aedad69ffa3d4590032539588
johniel.cenabre <johniel.cenabre@nextbank.ph>
2024-04-22 16:45:23 +0800

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-Beulahland Website
--- src/build.static.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const oldPath = path.join(__dirname, './out');
-const newPath = path.join(__dirname, '../');
-
-console.log("building static files...");
-
-fs.cpSync(oldPath, newPath, {recursive: true});
